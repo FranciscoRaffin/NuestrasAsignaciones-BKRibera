9cb43800fe124ebcda59408109a278e7040f76df
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-06-20 18:01:26 -0300

    Primer Commit

---
+++ funciones.js
@@ -0,0 +1,182 @@
+var conteo = 1;
+var fotograma = 0;
+var imagen = ["🕐","🕑","🕒","🕓","🕔","🕕","🕖","🕗","🕘","🕙","🕚","🕛"];
+
+function animacion() {
+    fotograma++
+    if (fotograma == imagen.length){
+        fotograma = 0
+    }
+    return `${imagen[fotograma]}`
+    
+}
+
+function mensajeDeCarga() {
+    if (conteo >= bernal_colab.length) {
+        return "👁 Asignaciones cargadas con éxito";
+    } else {
+        return `${animacion()} Buscando empleados: ${conteo} de ${bernal_colab.length}`;
+    }
+}
+
+function fetchAsignacion(legajo) {
+    return fetch(`https://api.asignaciones.com.ar/start.php?CP=BK&legajo=${legajo}`)
+    .then(response => response.text()) // Retorna el contenido como texto
+    .then(data => {
+        // Actualiza el contenido del elemento en el HTML
+        document.getElementById('contenido-fetch').innerText = mensajeDeCarga();
+        conteo++;
+        return data;
+    })
+    .catch(error => {
+        console.error('Error al obtener el contenido:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mostrarAsignacionesBtn = document.getElementById('mostrar-asignaciones-btn');
+    mostrarAsignacionesBtn.addEventListener('click', mostrarAsignaciones);
+});
+
+async function mostrarAsignacionesPorDia() {
+    if (conteo <= bernal_colab.length) {
+        try {
+            const asignacionesPorFecha = {}; // Objeto para almacenar las asignaciones agrupadas por fecha
+
+            for (const empleado of bernal_colab) {
+                const legajo = empleado[0];
+                const nombre = empleado[1];
+                const asignaciones = await fetchAsignacion(legajo); // Esperar a que se resuelva la promesa
+                const asignacionesData = JSON.parse(asignaciones);
+
+                if (asignacionesData && asignacionesData.asignaciones && asignacionesData.asignaciones.length > 0) {
+                    asignacionesData.asignaciones.forEach(asignacion => {
+                        const fecha = asignacion.fecha;
+                        if (!asignacionesPorFecha[fecha]) {
+                            asignacionesPorFecha[fecha] = [];
+                        }
+                        asignacionesPorFecha[fecha].push({ nombre, asignacion });
+                    });
+                }
+            }
+
+            const contenedor = document.getElementById('asignaciones-container');
+            contenedor.innerHTML = ''; // Limpiar el contenedor antes de agregar las tablas
+
+            // Ordenar las fechas
+            const fechasOrdenadas = Object.keys(asignacionesPorFecha).sort((a, b) => {
+                // Convertir las fechas de texto en objetos de fecha para compararlas
+                const fechaA = new Date(a.split(' ')[1]); // Seleccionar solo la parte DD/MM de la cadena
+                const fechaB = new Date(b.split(' ')[1]);
+                return fechaA - fechaB; // Ordenar en orden cronológico
+            });
+
+            // Crear tabla para cada fecha ordenada
+            fechasOrdenadas.forEach(fecha => {
+                const tabla = document.createElement('table');
+                tabla.classList.add('asignaciones-table');
+
+                // Crear un elemento h5 para mostrar la fecha
+                const fechaElemento = document.createElement('h5');
+                fechaElemento.textContent = fecha;
+                contenedor.appendChild(fechaElemento); // Agregar la fecha al contenedor
+
+                const encabezado = tabla.createTHead();
+                const filaEncabezado = encabezado.insertRow();
+                const encabezados = ['Nombre', 'Hora de entrada', 'Hora de salida'];
+                encabezados.forEach(encabezado => {
+                    const th = document.createElement('th');
+                    th.textContent = encabezado;
+                    filaEncabezado.appendChild(th);
+                });
+
+                const cuerpo = tabla.createTBody();
+
+                // Ordenar las asignaciones dentro de cada fecha por la hora de entrada
+                asignacionesPorFecha[fecha].sort((a, b) => {
+                    return a.asignacion.horaEntrada.localeCompare(b.asignacion.horaEntrada);
+                });
+
+                asignacionesPorFecha[fecha].forEach(asignacion => {
+                    const fila = cuerpo.insertRow();
+                    fila.insertCell().textContent = asignacion.nombre; // Mostrar nombre en lugar de legajo
+                    fila.insertCell().textContent = asignacion.asignacion.horaEntrada;
+                    fila.insertCell().textContent = asignacion.asignacion.horaSalida;
+                });
+
+                // Agregar la tabla al contenedor
+                contenedor.appendChild(tabla);
+            });
+
+        } catch (error) {
+            console.error('Error al mostrar las asignaciones:', error);
+        }
+    } else {
+        document.getElementById('contenido-fetch').innerText = "Las asignaciones ya fueron cargadas (para ver en el otro modo recarga la página)"
+    }
+}
+async function mostrarAsignaciones() {
+    if (conteo <= bernal_colab.length){
+    try {
+        for (const empleado of bernal_colab) {
+            const legajo = empleado[0];
+            const nombre = empleado[1];
+            const asignaciones = await fetchAsignacion(legajo); // Esperar a que se resuelva la promesa
+            mostrarAsignacionEnPagina(legajo, nombre, asignaciones);
+        }
+    } catch (error) {
+        console.error('Error al mostrar las asignaciones:', error);
+    }} else {
+        document.getElementById('contenido-fetch').innerText = "↻ Las asignaciones ya fueron cargadas: para ver en el otro modo recarga la página"
+    }
+}
+
+function mostrarAsignacionEnPagina(legajo, nombre, asignacionesJSON) {
+    const contenedor = document.getElementById('asignaciones-container');
+    const empleadoDiv = document.createElement('div');
+    const legajoTitulo = document.createElement('h4');
+    legajoTitulo.textContent = `${nombre} (${legajo})`;
+    empleadoDiv.appendChild(legajoTitulo);
+
+    // Convertir la cadena JSON de asignaciones en un objeto JavaScript
+    const asignaciones = JSON.parse(asignacionesJSON);
+
+    // Verificar si hay asignaciones disponibles
+    if (asignaciones && asignaciones.asignaciones && asignaciones.asignaciones.length > 0) {
+        // Crear tabla
+        const tabla = document.createElement('table');
+        tabla.classList.add('asignaciones-table');
+
+        // Encabezado de la tabla
+        const encabezado = tabla.createTHead();
+        const filaEncabezado = encabezado.insertRow();
+        const encabezados = ['Fecha', 'Hora de entrada', 'Hora de salida'];
+        encabezados.forEach(encabezado => {
+            const th = document.createElement('th');
+            th.textContent = encabezado;
+            filaEncabezado.appendChild(th);
+        });
+
+        // Cuerpo de la tabla
+        const cuerpo = tabla.createTBody();
+        asignaciones.asignaciones.forEach(asignacion => {
+            const fila = cuerpo.insertRow();
+            fila.insertCell().textContent = asignacion.fecha;
+            fila.insertCell().textContent = asignacion.horaEntrada;
+            fila.insertCell().textContent = asignacion.horaSalida;
+            //fila.insertCell().textContent = asignacion.tienda;
+        });
+
+        empleadoDiv.appendChild(tabla);
+    } else {
+        // Si no hay asignaciones disponibles, mostrar un mensaje
+        const errorMensaje = document.createElement('p');
+        errorMensaje.textContent = 'No hay asignaciones disponibles.';
+        empleadoDiv.appendChild(errorMensaje);
+    }
+
+    contenedor.appendChild(empleadoDiv);
+}
+
+const volverAlInicio = () => window.location.href = "index.html";
+const irASolicitarHorario = () => window.location.href = "pedir_horario.html";
---
+++ locales_data.js
@@ -0,0 +1,42 @@
+
+
+const bernal_colab = [
+//  |Legajo  [0]   |Nombre [1]
+    [ "131054" ,   "Francisco Raffin"     ],
+    [ "131043" ,   "Chamuel De Los Santos"],
+    [ "131065" ,   "Milagros Birtos"      ],
+    [ "131045" ,   "Rochio Marino"        ],
+    [ "131046" ,   "Lautaro Perez"        ],
+    [ "131047" ,   "Facundo Barta"        ],
+    [ "131053" ,   "Franco Delgado"       ],
+    [ "131018" ,   "Rocio Miranda"        ],
+    [ "131021" ,   "Alejo Aguirre"        ],
+    [ "B9830"  ,   "Erica Rosso"          ],
+    [ "131059" ,   "Natalie Fernandez"    ],
+    [ "131057" ,   "Florencia Ayala"      ],
+    [ "95085"  ,   "Agustin Cornejo"      ],
+    [ "131005" ,   "Lautaro  Lezcano"     ],
+    [ "61379"  ,   "Agustina Amodeo"      ],
+    [ "131062" ,   "Natanael Gomez"       ],
+    [ "131067" ,   "Danilo Torres"        ],
+    [ "131066" ,   "Fray Alucena"         ],
+    [ "131055" ,   "Luna Zugasti"         ],
+    [ "B6654"  ,   "Yamila Bizarap"       ],
+    [ "95140"  ,   "Milena"               ]
+
+
+];
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
